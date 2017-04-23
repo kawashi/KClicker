1c689f265ef34be31761bef243e686b58960ce42
Marihuana <allen@SK0025.local>
2017-04-23 16:27:23 +0900

    クリックして遊ぶ機能を追加

--- app/src/main/java/com/example/allen/kclicker/GameActivity.java
+++ app/src/main/java/com/example/allen/kclicker/GameActivity.java
@@ -3,12 +3,31 @@ package com.example.allen.kclicker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        final KClicker kclicker = new KClicker(10);
+        TextView countText = (TextView)findViewById(R.id.count);
+        countText.setText("あと" + kclicker.getCount() + "回");
+
+        findViewById(R.id.clickButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                kclicker.click();
+                TextView countText = (TextView) findViewById(R.id.count);
+
+                if(kclicker.isEnd()) {
+                    countText.setText("クリア！");
+                } else {
+                    countText.setText("あと" + kclicker.getCount() + "回");
+                }
+            }
+        });
 
         findViewById(R.id.prevButton).setOnClickListener(new View.OnClickListener(){
             @Override
@@ -17,4 +36,18 @@ public class GameActivity extends AppCompatActivity {
             }
         });
     }
+}
+
+
+class KClicker {
+    private int clickCount;
+
+    KClicker(int count){
+        setCount(count);
+    }
+
+    void setCount(int count) { clickCount = count; }
+    int  getCount() { return clickCount; }
+    void click() { clickCount -= 1; }
+    boolean isEnd() { return clickCount <= 0; }
 }
